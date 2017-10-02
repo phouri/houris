80ab52923d0b0a99fe0eab702743a94560c2ba57
pini <pini.h@dynamicyield.com>
2017-10-02 11:38:00 +0300

    Deploy

--- circleci-deploy.sh
+++ circleci-deploy.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 echo $GCLOUD_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
-# Install gcloud cause the circle server already has an old version of it installed: https://cloud.google.com/sdk/#deb
-# Create an environment variable for the correct distribution
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-# Add the Cloud SDK distribution URI as a package source
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-# Import the Google Cloud public key
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-# Update and install the Cloud SDK
-sudo apt-get update && sudo apt-get install google-cloud-sdk
-
-export PATH="/usr/lib/google-cloud-sdk/bin:$PATH"
-
-sudo gcloud --quiet components update
-sudo gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-sudo gcloud config set project hourisnetwork
-sudo gcloud -q app deploy app.yaml --promote 
+# Install gcloud
+curl -L -o google-cloud-sdk.zip https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip \
+unzip google-cloud-sdk.zip
+rm google-cloud-sdk.zip
+google-cloud-sdk/install.sh --usage-reporting=false --bash-completion=true --path-update=true --rc-path=/.bashrc
+gcloud --quiet components update
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud config set project hourisnetwork
+gcloud -q app deploy app.yaml --promote 

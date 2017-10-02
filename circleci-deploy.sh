a3c94a7ce1ea1e389a797805dbbbd4d486ece644
pini <pini.h@dynamicyield.com>
2017-10-02 11:41:35 +0300

    Deploy

--- circleci-deploy.sh
+++ circleci-deploy.sh
@@ -4,7 +4,7 @@ echo $GCLOUD_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key
 curl -L -o google-cloud-sdk.zip https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip
 unzip google-cloud-sdk.zip
 rm google-cloud-sdk.zip
-google-cloud-sdk/install.sh --usage-reporting=false --bash-completion=true --path-update=true --rc-path=/.bashrc
+sudo google-cloud-sdk/install.sh --usage-reporting=false --bash-completion=true --path-update=true --rc-path=/.bashrc
 gcloud --quiet components update
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project hourisnetwork
